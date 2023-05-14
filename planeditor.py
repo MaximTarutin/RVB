223a50798daa7601ac868cd04b83f035a846c489
MaximTarutin <mtarut@mail.ru>
2023-05-14 22:05:23 +0300

    Сделана проверка корректности заполнения данных для записи их в таблицу

--- planeditor.py
+++ planeditor.py
@@ -1,6 +1,8 @@
 """ Редактор планировщика """
+# ДОДЕЛАТЬ ФУНКЦИЮ СОХРАНЕНИЯ В БАЗУ ДАННЫХ
+
 import sys
-from PySide6.QtWidgets import (QMainWindow, QApplication)
+from PySide6.QtWidgets import (QMainWindow, QApplication, QMessageBox)
 from PySide6.QtCore import (QDate, Qt)
 from PySide6.QtSql import (QSqlTableModel, QSqlQuery)
 import ui_planeditor
@@ -20,6 +22,7 @@ class PlanEditor(QMainWindow):
         self.init_plan_window()
 
         self.ui.pushButton_return.clicked.connect(self.__closeWindow)        # Закрываем это окно
+        self.ui.pushButton_add.clicked.connect(self.__data_to_base)          # Заносим данные в базу данных
 
 #------------------------ Инициализация планировщика ---------------------------------------
 
@@ -65,6 +68,34 @@ class PlanEditor(QMainWindow):
 
     def __closeWindow(self):
         self.close()
+
+#------------------------ Вносим данные в базу данных -------------------------------------
+
+    def __data_to_base(self):
+        self.__check_to_data()
+
+#------------------------ Проверка все ли поля заполнены перед внесением в базу ------------
+
+    def __check_to_data(self):
+        if self.ui.comboBox.currentIndex() == 0:
+            self.__error_message("\n\n\tНе выбран сотрудник !!!\t\t\n\n")
+            return
+        if self.ui.comboBox_2.currentIndex() == 0:
+            self.__error_message("\n\n\tНе выбрана станция или перегон !!!\t\t\n\n")
+            return
+        if len(self.ui.textEdit_plan.toPlainText()) < 1 or \
+               self.ui.textEdit_plan.toPlainText().isspace():
+            self.__error_message("\n\n\tНе заполнено поле работы !!!\t\t\n\n")
+            return
+
+#------------------------ Окно сообщения обошибке ------------------------------------------
+
+    def __error_message(self, message):
+        self.msg = QMessageBox()
+        self.msg.setWindowTitle('Внимание!')
+        self.msg.setStyleSheet("background-color: pink; font: 700 italic 16pt 'Times New Roman'")
+        self.msg.setText(message)
+        self.msg.exec()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
