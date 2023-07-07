dbab58377190b24c3693fbd1fce0f7871d8ec6b6
MaximTarutin <mtarut@mail.ru>
2023-07-07 15:21:00 +0300

    выполнено: 1) отображение данных в таблице по заданному фильтру в модуле comments_view.py

--- comments_view.py
+++ comments_view.py
@@ -28,6 +28,11 @@ class Comments_View(QMainWindow):
         self.initial()
 
         self.ui.edit_checkBox.stateChanged.connect(self.check_checkBox)
+        self.ui.commis_Box.currentIndexChanged.connect(self.__data_filter)
+        self.ui.station_Box.currentIndexChanged.connect(self.__data_filter)
+        self.ui.auditor_Box.currentIndexChanged.connect(self.__data_filter)
+        self.ui.worker_Box.currentIndexChanged.connect(self.__data_filter)
+        self.ui.performance_Box.currentIndexChanged.connect(self.__data_filter)
 
 #------------------------- Инициализация -------------------------------
 
@@ -80,6 +85,52 @@ class Comments_View(QMainWindow):
             self.ui.commis_Box.addItem(i)
         del new_list_kommis
         del list_kommis
+
+        self.ui.tableView.verticalHeader().hide()
+        self.ui.tableView.setColumnHidden(0, True)
+        self.model.setHeaderData(1, Qt.Horizontal, "№")
+        self.model.setHeaderData(2, Qt.Horizontal, "Дата")
+        self.model.setHeaderData(3, Qt.Horizontal, "Комиссия")
+        self.model.setHeaderData(4, Qt.Horizontal, "Станция")
+        self.model.setHeaderData(5, Qt.Horizontal, "Проверяющий")
+        self.model.setHeaderData(6, Qt.Horizontal, "Выявленные замечания")
+        self.model.setHeaderData(7, Qt.Horizontal, "Срок")
+        self.model.setHeaderData(8, Qt.Horizontal, "Исполнитель")
+        self.model.setHeaderData(9, Qt.Horizontal, "Выполнение")
+        self.model.setHeaderData(10, Qt.Horizontal, "Дата")
+        self.model.setHeaderData(11, Qt.Horizontal, "Отметка о выполнении")
+        self.model.setHeaderData(12, Qt.Horizontal, "Фото")
+        self.model.select()
+
+        self.ui.tableView.setColumnWidth(1, 20)
+        self.ui.tableView.setColumnWidth(2, 100)
+        self.ui.tableView.setColumnWidth(3, 160)
+        self.ui.tableView.setColumnWidth(4, 160)
+        self.ui.tableView.setColumnWidth(5, 160)
+        self.ui.tableView.setColumnWidth(6, 400)
+        self.ui.tableView.setColumnWidth(7, 100)
+        self.ui.tableView.setColumnWidth(8, 160)
+        self.ui.tableView.setColumnWidth(9, 160)
+        self.ui.tableView.setColumnWidth(10,100)
+        self.ui.tableView.setColumnWidth(11,400)
+        self.ui.tableView.setColumnWidth(12, 20)
+
+        self.ui.tableView.resizeRowsToContents()                        # Содержимое вписывается в ячейку
+        self.__data_filter()
+
+
+#------------------------- отображаем данные в таблице по заданному фильтру ----------------------------------------
+
+    def __data_filter(self):
+        commis = self.ui.commis_Box.currentText()
+        station = self.ui.station_Box.currentText()
+        auditor = self.ui.auditor_Box.currentText()
+        worker = self.ui.worker_Box.currentText()
+        performance = self.ui.performance_Box.currentText()
+
+        self.model.setFilter('''kommis like "%'''+commis+'''%" AND station like "%'''+station+'''%" AND 
+                                auditor like "%'''+auditor+'''%" AND worker like "%'''+worker+'''%" AND 
+                                performance like "'''+performance+'''%"''')
 
 
 # ------------------------- Получаем сигнал со значением режима программы (админ или юзер) ---------------------------
